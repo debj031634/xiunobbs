0d881f3017e79b06d4e5913474e52549bfd5061f
HUYANG-WIN11\hyang <debj031634@163.com>
2023-07-28 19:31:37 +0800

    修复验证码模块在登录注册页面不生效的问题

--- plugin/xn_vcode/hook/model_misc_end.php
+++ plugin/xn_vcode/hook/model_misc_end.php
@@ -2,9 +2,15 @@
 
 function vcode_on($kv_code) {
 	global $gid, $user, $time;
-	if(empty($user)) return FALSE;
+
+	$utime = $user['create_date'];
+	if(empty($user)) $utime=$time;
+
+	$r=false;
 	$r = (!empty($kv_code['vcode_gids']) && in_array($gid, $kv_code['vcode_gids'])) &&
-		(empty($kv_code['vcode_create_date_day_less']) || $time - $user['create_date'] < $kv_code['vcode_create_date_day_less'] * 86400);
+		(empty($kv_code['vcode_create_date_day_less']) || $time - $utime < $kv_code['vcode_create_date_day_less'] * 86400);
+		
+		
 	return $r;
 }
 
